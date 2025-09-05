6da7c7180ef7584d361a1e2e9520de2680c436e1
YazanHasanat <yazenhasanat@gmail.com>
2025-09-05 23:58:22 +0300

    demo

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_URL).then(
-  () => {
-    console.log("DB Ready To Use");
-  },
-  (err) => {
-    console.log(err);
-  }
-);
+require("dotenv").config(); 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("DB Ready To Use");
+})
+.catch((err) => {
+  console.error("DB Connection Error:", err);
+});
